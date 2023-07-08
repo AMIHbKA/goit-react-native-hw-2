d4d09705efdf2136f46e6f75de76d2723f4f0d8a
AMIHbKA <95082331+AMIHbKA@users.noreply.github.com>
2023-07-08 09:05:58 +0300

    some fixes

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet } from "react-native";
-import Background from "./src/components/Background/Background";
-import { LoginScreen } from "./src/Screens/LoginScreen/LoginScreen";
 import { LoginScreenAdaptive } from "./src/Screens/LoginScreen/LoginScreenAdaptive";
 import { PostsScreen } from "./src/Screens/PostsScreen/PostsScreen";
-import { RegistrationScreen } from "./src/Screens/RegistrationScreen/RegistrationScreen";
 import { RegistrationScreenAdaptive } from "./src/Screens/RegistrationScreen/RegistrationScreenAdaptive";
-import { pixels } from "./src/utility/adptivePixels";
 
 export default function App() {
   return (
     <>
       <StatusBar />
-      {/* <RegistrationScreen /> */}
-      {/* <RegistrationScreenAdaptive /> */}
-      <LoginScreenAdaptive />
-      {/* <Background /> */}
-      {/* <LoginScreen /> */}
+      <RegistrationScreenAdaptive />
+      {/* <LoginScreenAdaptive /> */}
       {/* <PostsScreen /> */}
     </>
   );
